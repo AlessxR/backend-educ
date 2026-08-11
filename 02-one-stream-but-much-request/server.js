b113c582310c01ea94ff51524256be76d770b1b9
AlessxR <alexxsrs.al@gmail.com>
2026-08-11 12:49:57 +0300

    02 - async/await with response, request, asynchonious moves, readfile, delay

---
+++ 02-one-stream-but-much-request/server.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+
+const delay = (ms) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+        }, ms)
+    })
+};
+const readFile = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
+}
+
+const server = http.createServer(async (request, response) => {
+    switch (request.url) {
+        case '/home': {
+            try {
+                const data = await readFile('pages/home.html');
+                response.write(data);
+                break;
+            } catch (e) {
+                response.write('something wrong...');
+                break;
+            }
+        }
+        case '/about': {
+            await delay(3000);
+            response.write('About Page');
+            break;
+        }
+        default: {
+            response.write('404 not found');
+        }
+    }
+
+    response.end();
+});
+
+server.listen(3003);
